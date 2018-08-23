fc2648fa885ded7d5a438453285206cf9359ce54
Stephen Pangburn II <stephen.pangburn.ii@gmail.com>
2018-08-23 14:02:03 -0700

    Say goodbye to you

--- circle.rb
+++ circle.rb
@@ -8,7 +8,7 @@ class Circle
     "Hello, I'm a #{@color} circle!"
   end
 
-  def say_goodbye
+  def say_goodbye_to_you
     "Goodbye! I'm a #{@color} circle! And I have a radius of #{@radius} :))))))"
   end
 end
